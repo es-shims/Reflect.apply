0829ab23a0a8efe4332a4b599dd9a3066617e6ad
Jordan Harband <ljharb@gmail.com>
2021-10-03 23:56:00 -0700

    [Fix] install `Reflect.apply` with correct property descriptor

--- shim.js
+++ shim.js
@@ -1,16 +1,28 @@
 'use strict';
 
-var define = require('define-properties');
+var CreateMethodProperty = require('es-abstract/2021/CreateMethodProperty');
+var DefinePropertyOrThrow = require('es-abstract/2021/DefinePropertyOrThrow');
 var globalThis = require('globalthis')();
 
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimReflectApply() {
 	if (typeof Reflect === 'undefined') {
-		globalThis.Reflect = {};
+		var R = {};
+		if (typeof Symbol === 'function' && Symbol.toStringTag) {
+			DefinePropertyOrThrow(R, Symbol.toStringTag, {
+				'[[Configurable]]': true,
+				'[[Enumerable]]': false,
+				'[[Value]]': 'Reflect',
+				'[[Writable]]': false
+			});
+		}
+		CreateMethodProperty(globalThis, 'Reflect', R);
 	}
 
 	var polyfill = getPolyfill();
-	define(Reflect, { apply: polyfill });
+	if (polyfill !== Reflect.apply) {
+		CreateMethodProperty(Reflect, 'apply', polyfill);
+	}
 	return polyfill;
 };
