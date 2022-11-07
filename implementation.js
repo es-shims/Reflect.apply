9cf06cd1d732ed02fd913b045e621c043bbfcda1
Jordan Harband <ljharb@gmail.com>
2022-11-06 23:01:00 -0800

    [Deps] update `define-properties`, `es-abstract`, `globalthis`

--- implementation.js
+++ implementation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Call = require('es-abstract/2021/Call');
+var Call = require('es-abstract/2022/Call');
 
 module.exports = function apply(target, thisArgument, argumentsList) {
 	return Call(target, thisArgument, argumentsList);
